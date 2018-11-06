51d9bafa0a56e0c31de69c0b6436fc0d60161f2a
Rainee4563 <calvin4084@gmail.com>
2018-11-05 23:58:38 -0800

    updated dependances and remove code

--- index.js
+++ index.js
@@ -9,102 +9,116 @@ bot.on("ready", function () {
     console.log("Ready");
 });
 
-// start twitch
-var tmi = require("tmi.js");
-var options = {
-    options: {
-        debug: true
-    },
-    connection: {
-        recconect: true
-    },
-    identity: {
-        username: "DisQuote",
-        password: (config.oauth)
-    },
-    channels: ["#brettdoesgaming"]
-};
+bot.user.setActivity("Ready to serve!");
 
-var client = new tmi.client(options);
+bot.on("message", async message => {
+    // This event will run on every single message received, from any channel or DM.
 
-bot.on("message", function (message) {
-    if (message.author.equals(bot.user)) return;
+    // It's good practice to ignore other bots. This also makes your bot ignore itself
+    // and not get into a spam loop (we call that "botception").
+    if (message.author.bot) return;
 
-    if (!message.content.startsWith(PREFIX)) return;
+    // Also good practice to ignore any message that does not start with our prefix, 
+    // which is set in the configuration file.
+    if (message.content.indexOf(config.prefix) !== 0) return;
 
-    var args = message.content.substring(PREFIX.length).split(" ")
+    // Here we separate our "command" name, and our "arguments" for the command. 
+    // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+    // command = say
+    // args = ["Is", "this", "the", "real", "life?"]
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
 
-    switch (args[0].toLowerCase()) {
-        case "nep":
-            message.channel.send("Nepu!");
-            break;
-        case "brett":
-            message.channel.send("I'm Bread")
-            break;
-        case "info":
-            message.channel.send("I'm DisQuote! Created by @MrMarioBrosFTW#5487")
-            break;
-        case "embed":
-            var embed = new Discord.RichEmbed()
-                .addField("Test Title", "Test Description", true)
-                .addField("Test Title2", "Test Description2", true)
-                .addField("Test Title3", "Test Description3")
-                .setColor(0x00FFFF)
-                .setFooter("Testing stuff is fun, Life is fun")
-                .setThumbnail(message.author.avatarURL)
-            message.channel.sendEmbed(embed);
-            break;
-        case "noticeme":
-            message.channel.send(message.author.toString() + " You have been Noticed!!!")
-            break;
+    // Let's go with a few common example commands! Feel free to delete or change those.
 
-        default:
-            message.channel.send("Hey Idiot... Wrong Command")
+    if (command === "ping") {
+        // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+        // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+        const m = await message.channel.send("Ping?");
+        m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
     }
+
+    if (command === "say") {
+        // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+        // To get the "message" itself we join the `args` back into a string with spaces: 
+        const sayMessage = args.join(" ");
+        // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+        message.delete().catch(O_o => { });
+        // And we get the bot to say the thing: 
+        message.channel.send(sayMessage);
+    }
+
+    if (command === "kick") {
+        // This command must be limited to mods and admins. In this example we just hardcode the role names.
+        // Please read on Array.some() to understand this bit: 
+        // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
+        if (!message.member.roles.some(r => ["Administrator", "Moderator"].includes(r.name)))
+            return message.reply("Sorry, you don't have permissions to use this!");
+
+        // Let's first check if we have a member and if we can kick them!
+        // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
+        // We can also support getting the member by ID, which would be args[0]
+        let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+        if (!member)
+            return message.reply("Please mention a valid member of this server");
+        if (!member.kickable)
+            return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+
+        // slice(1) removes the first part, which here should be the user mention or ID
+        // join(' ') takes all the various parts to make it a single string.
+        let reason = args.slice(1).join(' ');
+        if (!reason) reason = "No reason provided";
+
+        // Now, time for a swift kick in the nuts!
+        await member.kick(reason)
+            .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+        message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+
+    }
+
+    if (command === "ban") {
+        // Most of this command is identical to kick, except that here we'll only let admins do it.
+        // In the real world mods could ban too, but this is just an example, right? ;)
+        if (!message.member.roles.some(r => ["Administrator"].includes(r.name)))
+            return message.reply("Sorry, you don't have permissions to use this!");
+
+        let member = message.mentions.members.first();
+        if (!member)
+            return message.reply("Please mention a valid member of this server");
+        if (!member.bannable)
+            return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+
+        let reason = args.slice(1).join(' ');
+        if (!reason) reason = "No reason provided";
+
+        await member.ban(reason)
+            .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+        message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
+    }
+
+    if (command === "purge") {
+        // This command removes all messages from all users in the channel, up to 100.
+
+        // get the delete count, as an actual number.
+        const deleteCount = parseInt(args[0], 10);
+
+        // Ooooh nice, combined conditions. <3
+        if (!deleteCount || deleteCount < 2 || deleteCount > 100)
+            return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+
+        // So we get our messages, and delete them. Simple enough, right?
+        const fetched = await message.channel.fetchMessages({ count: deleteCount });
+        message.channel.bulkDelete(fetched)
+            .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+    }
+
+    if(message.permissions)
 });
+
+
+
 
 
 bot.login(config.distoken);
 
-//connect client to the server...
-client.connect().then(function (data) {
-    client.action(channel, "Hello World!").then(function (data) {
-        //data returns [channel]
-    }).catch(function (err) {
-        //
-    });
-}).catch(function (er) {
 
-});
-
-client.on("chat", function (channel, userstate, message, self) {
-    if (self) return;
-    //commands go here
-    if (message.startsWith(";ping")) {
-        var channel = bot.channels.find('name', 'bdg-chat');
-        channel.send('Pont');
-        console.log(bot.channels.first());
-    };
-    if (message.startsWith("!!nep")) {
-        client.say("brettdoesgaming", "Nepu!");
-    };
-    if (message.startsWith("!!brett")) {
-        client.say("brettdoesgaming", "I'm Bread");
-    };
-    if (message.startsWith("!!info")) {
-        client.say("brettdoesgaming", "Hey I'm DisQuote a bot created by MrMarioBrosFTW for the purposes of tracking quotes and clips in this channel");
-    };
-    if (message.startsWith("!!clip")) {
-        client.say("brettdoesgaming", "Hey don't worry about manually adding it in theres another bot working along side me to auto send them into discord so just post it...");
-    };
-    if (message.startsWith("!!quote")) {
-        var quote = bot.channels.find('name', 'livestream_quotes');
-        var username = userstate['username'];
-        quote.send('**' + "Quoted By " + username + '**: ' + message);
-    };
-   
-    //relays twitch chat to discord server
-    var channel = bot.channels.find('name', 'bdg-chat');
-    var username = userstate['username'];
-    channel.send('**' + username + '**: ' + message);
-});
